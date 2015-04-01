34b8aab786682c64420c34d91531821e44a86f50
Spiros <smouzakitis@epu.ntua.gr>
2015-04-02 00:28:49 +0300

    Plain NER Entity Service

--- molly/ner.py
+++ molly/ner.py
@@ -1,8 +1,21 @@
 ﻿#Named Entity Recognition Library
+import lists
 
 def recognize(text):
     #dummy implementation
-    recognizedEntity = ''
-    if (text == 'Greece'): recognizedEntity = 'Country'
-    if (text == '2011'): recognizedEntity = 'Year'
+    text = text.lower()
+    recognizedEntity = 'Unknown Type'
+    #Check if Country
+    if any(text == s for s in lists.countries_list):
+        recognizedEntity = 'Country'
+        return recognizedEntity;
+    #Check if Year
+    if lists.is_int(text):
+       number = int(text)
+       if (number > 1900 and number < 2100): 
+           recognizedEntity = 'Year'
+           return recognizedEntity;
+    #Check if Date
+    if (lists.is_date(text)) :
+          recognizedEntity = 'Date'
     return recognizedEntity;
